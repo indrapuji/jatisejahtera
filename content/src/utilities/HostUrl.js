82d55a4c84f29877422142477aed9078883a0768
Indra Puji Novirwan <indrapuji@gmail.com>
2023-05-12 10:53:34 +0700

    change host

--- content/src/utilities/HostUrl.js
+++ content/src/utilities/HostUrl.js
@@ -1,4 +1,3 @@
-// const url = 'https://dev.jatisejahtera.or.id'
-// const url = 'https://api.jatisejahtera.or.id'
-const url = 'http://localhost:3001';
+const url = 'https://jati.pasangbajaringan.id';
+// const url = 'http://localhost:3001';
 export default url;
