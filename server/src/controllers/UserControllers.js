ed810365b0216a61846bbe1684da2bb10ba4b584
Indra Puji Novirwan <indrapuji@gmail.com>
2023-05-24 22:16:45 +0700

    add role to register member

--- server/src/controllers/UserControllers.js
+++ server/src/controllers/UserControllers.js
@@ -78,7 +78,7 @@ class UserControllers {
   };
   static userRegister = async (req, res, next) => {
     try {
-      const {name, nip, email, username, password, regional} = req.body;
+      const {name, nip, email, username, password, regional, role} = req.body;
       if (!name || !nip || !email || !username || !password) {
         throw createErrors(400, 'Input all require field');
       }
