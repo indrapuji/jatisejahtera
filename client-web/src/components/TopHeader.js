ee70883a0883ce7e5bb551ab7a1cdef7c7f0edc3
Indra Puji Novirwan <indrapuji@gmail.com>
2023-05-24 17:02:23 +0700

    running text

--- client-web/src/components/TopHeader.js
+++ client-web/src/components/TopHeader.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 function TopHeader() {
   return (
-    <div className='bg-emerald-600 px-4 py-3 text-white hidden md:block'>
-      <p className='text-center text-sm text-black font-medium'>Yayasan Kesejahteraan Pegawai Perum Perhutani</p>
+    <div className='bg-emerald-600 px-4 py-3 text-white hidden md:block' id='scroll-container'>
+      <p className='text-center text-sm text-black font-medium' id='scroll-text'>
+        Yayasan Kesejahteraan Pegawai Perum Perhutani JATISEJAHTERA
+      </p>
     </div>
   );
 }
