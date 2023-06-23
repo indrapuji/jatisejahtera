ba40fe02a4d1aad83539a9868c81ee9c55e7576e
Indra Puji Novirwan <indrapuji@gmail.com>
2023-06-23 16:20:02 +0700

    scaling iamge news

--- client-web/src/components/NewsLeft.js
+++ client-web/src/components/NewsLeft.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 function NewsLeft({data}) {
-  console.log(data);
+  console.log(data)
   return (
     <div className='px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20'>
       <div className='grid gap-10 lg:grid-cols-2'>
@@ -10,11 +10,11 @@ function NewsLeft({data}) {
           <p className='mb-6 text-gray-900'>{data.desc}</p>
         </div>
         <div>
-          <img className='object-cover w-full h-56 rounded shadow-lg sm:h-96' src={data.image_url} alt='news' />
+          <img className='object-cover w-full h-48 rounded shadow-lg sm:h-72' src={data.image_url} alt='news' />
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default NewsLeft;
+export default NewsLeft
