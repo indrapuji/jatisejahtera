aa4ef777282536e4645a632e5c2cbe4d71db9849
Indra Puji Novirwan <indrapuji@gmail.com>
2023-07-03 21:47:37 +0700

    change news

--- client-web/src/pages/news/index.js
+++ client-web/src/pages/news/index.js
@@ -1,30 +1,31 @@
-import React, {useEffect, useState} from 'react';
-import NewsRight from '@components/NewsRight';
-import NewsLeft from '@components/NewsLeft';
-import Layout from '@components/Layout';
-import Navigation from '@components/Navigation';
-import Footer from '@components/Footer';
+import React, {useEffect, useState} from 'react'
+// import NewsRight from '@components/NewsRight'
+// import NewsLeft from '@components/NewsLeft'
+import Layout from '@components/Layout'
+import Navigation from '@components/Navigation'
+import Footer from '@components/Footer'
 
-import axios from 'axios';
+import axios from 'axios'
 
 function index() {
-  const [newsData, setNewsData] = useState();
+  const [newsData, setNewsData] = useState()
 
   useEffect(() => {
-    getData();
-  }, []);
+    getData()
+  }, [])
 
   const getData = async () => {
     try {
       const {data} = await axios({
         method: 'GET',
-        url: `${process.env.API_URL}/content?category=berita&status=true`,
-      });
-      setNewsData(data);
+        url: `${process.env.API_URL}/content?category=berita&status=true`
+      })
+      setNewsData(data)
+      console.log(data)
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   return (
     <Layout pageTitle={'BERITA || JATISEJAHTERA'}>
@@ -52,14 +53,30 @@ function index() {
             Seputar Informasi Yayasan JATISEJAHTERA
           </p>
         </div>
-        {newsData &&
+        {/* {newsData &&
           newsData.map((item, index) =>
             index % 2 === 0 ? <NewsRight key={item.id} data={item} /> : <NewsLeft key={item.id} data={item} />,
-          )}
+          )} */}
+        <div className='px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20'>
+          <div className='grid gap-5 row-gap-5 mb-8 lg:grid-cols-4 sm:grid-cols-2'>
+            {newsData &&
+              newsData.map((item, index) => (
+                <div key={item.id} className='flex flex-col h-full'>
+                  <img src={item.image_url} className='object-cover w-full h-48' alt='' />
+                  <div className='flex-grow border border-t-0 rounded-b'>
+                    <div className='p-5'>
+                      <h6 className='mb-2 font-semibold leading-5'>{item.title}</h6>
+                      <p className='text-sm text-gray-900'>{item.desc}</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
         <Footer />
       </div>
     </Layout>
-  );
+  )
 }
 
-export default index;
+export default index
