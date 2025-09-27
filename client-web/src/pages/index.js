cc9a6dd3ced513e610afeba40097d2e95ac39079
Indra Puji Novirwan <indrapuji@gmail.com>
2025-09-27 16:24:01 +0700

    new start

--- client-web/src/pages/index.js
+++ client-web/src/pages/index.js
@@ -1,23 +1,23 @@
-import React, {useEffect, useState} from 'react';
-import Layout from '@components/Layout';
-import Navigation from '@components/Navigation';
-import TopHeader from '@components/TopHeader';
-import Carousel from '@components/Carousel';
-import Program from '@components/Program';
-import News from '@components/News';
-import Testimonial from '@components/Testimonial';
-import Contact from '@components/Contact';
-import Footer from '@components/Footer';
-import Claim from '@components/Claim';
-import axios from 'axios';
-import Modal from '@components/Modal';
-import Realisasi from '@components/Realisasi';
-import Galery from '@components/Galery';
-import UpdateNotif from '@components/UpdateNotif';
-import ErrorScreen from '@components/ErrorScreen';
+import React, { useEffect, useState } from "react";
+import Layout from "@components/Layout";
+import Navigation from "@components/Navigation";
+import TopHeader from "@components/TopHeader";
+import Carousel from "@components/Carousel";
+import Program from "@components/Program";
+import News from "@components/News";
+import Testimonial from "@components/Testimonial";
+import Contact from "@components/Contact";
+import Footer from "@components/Footer";
+import Claim from "@components/Claim";
+import axios from "axios";
+import Modal from "@components/Modal";
+import Realisasi from "@components/Realisasi";
+import Galery from "@components/Galery";
+import UpdateNotif from "@components/UpdateNotif";
+import ErrorScreen from "@components/ErrorScreen";
 
 export default function Home() {
-  const isBrowser = () => typeof window !== 'undefined';
+  const isBrowser = () => typeof window !== "undefined";
 
   const [modalShow, setModalShow] = useState(false);
   const [bannerShow, setBannerShow] = useState();
@@ -26,14 +26,14 @@ export default function Home() {
 
   useEffect(() => {
     if (!isBrowser()) return;
-    window.scrollTo({top: 0, behavior: 'smooth'});
+    window.scrollTo({ top: 0, behavior: "smooth" });
     getData();
   }, []);
 
   const getData = async () => {
     try {
-      const {data} = await axios({
-        method: 'GET',
+      const { data } = await axios({
+        method: "GET",
         url: `${process.env.API_URL}/content?category=banner&status=true`,
       });
       if (data.length > 0) {
@@ -41,13 +41,13 @@ export default function Home() {
         setModalShow(true);
       }
       const dataGaleri = await axios({
-        method: 'GET',
+        method: "GET",
         url: `${process.env.API_URL}/content?category=galeri&status=true`,
       });
       setGaleriData(dataGaleri.data);
       console.log(dataGaleri.data);
       const dataNews = await axios({
-        method: 'GET',
+        method: "GET",
         url: `${process.env.API_URL}/content?category=berita&status=true`,
       });
       setNewsData(dataNews.data);
@@ -57,11 +57,15 @@ export default function Home() {
   };
 
   return (
-    <Layout pageTitle={'HOME || JATISEJAHTERA'}>
-      {/* <TopHeader />
+    <Layout pageTitle={"HOME || JATISEJAHTERA"}>
+      <TopHeader />
       <UpdateNotif />
       <Navigation />
-      <Modal onShow={modalShow} onHide={() => setModalShow(false)} picture={bannerShow} />
+      <Modal
+        onShow={modalShow}
+        onHide={() => setModalShow(false)}
+        picture={bannerShow}
+      />
       <Carousel />
       <Program />
       <News data={newsData && newsData.slice(0, 3)} />
@@ -70,8 +74,8 @@ export default function Home() {
       <Claim />
       <Realisasi hideHeader={false} />
       <Galery data={galeriData && galeriData.slice(0, 4)} showMore />
-      <Footer /> */}
-      <ErrorScreen/>
+      <Footer />
+      {/* <ErrorScreen/> */}
     </Layout>
   );
 }
