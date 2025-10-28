091f60b6c67b0e1a65e64eb22dc5a9479822ce5f
Indra Puji Novirwan <indrapuji@gmail.com>
2025-10-28 23:16:02 +0700

    new server

--- client-web/src/pages/data/pendidikan.js
+++ client-web/src/pages/data/pendidikan.js
@@ -1,43 +1,48 @@
-import React, {useEffect, useState} from 'react'
-import Navigation from '@components/Navigation'
-import Footer from '@components/Footer'
-import Image from 'next/image'
-import Layout from '@components/Layout'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import Navigation from "@components/Navigation";
+import Footer from "@components/Footer";
+import Image from "next/image";
+import Layout from "@components/Layout";
+import axios from "axios";
 
 export default () => {
-  const [dataPendidikan, setDataPendidikan] = useState()
+  const [dataPendidikan, setDataPendidikan] = useState();
 
   useEffect(() => {
-    getDataPendidikan()
-  }, [])
+    getDataPendidikan();
+  }, []);
 
   const getDataPendidikan = async () => {
     try {
-      const {data} = await axios({
-        method: 'GET',
-        url: `${process.env.API_URL}/content?category=pendidikan&status=true`
-      })
-      console.log(data)
-      setDataPendidikan(data)
+      const { data } = await axios({
+        method: "GET",
+        url: `${process.env.API_URL}/content?category=pendidikan&status=true`,
+      });
+      console.log(data);
+      setDataPendidikan(data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
   return (
-    <Layout pageTitle={'REALISASI PENDIDIKAN || JATISEJAHTERA'}>
+    <Layout pageTitle={"REALISASI PENDIDIKAN || JATISEJAHTERA"}>
       <Navigation />
-      <div className='flex flex-col justify-between'>
-        <section className='bg-white dark:bg-gray-900'>
-          <div className='container flex flex-col items-center px-4 py-12 mx-auto text-center'>
-            <h2 className='max-w-2xl mx-auto text-2xl font-semibold tracking-tight text-gray-800 xl:text-3xl dark:text-white'>
+      <div className="flex flex-col justify-between">
+        <section className="bg-white dark:bg-gray-900">
+          <div className="container flex flex-col items-center px-4 py-12 mx-auto text-center">
+            <h2 className="max-w-2xl mx-auto text-2xl font-semibold tracking-tight text-gray-800 xl:text-3xl dark:text-white">
               Realisasi Pendidikan
             </h2>
             {dataPendidikan &&
               dataPendidikan.map((item) => (
-                <div key={item.id} className='max-w-4xl my-6 mb-4'>
-                  <Image src={item.image_url} width={1000} height={700} alt='pop up' />
-                  <p className='font-bold px-2 py-8'>{item.title}</p>
+                <div key={item.id} className="max-w-4xl my-6 mb-4">
+                  <Image
+                    src={item.image_url}
+                    width={1000}
+                    height={700}
+                    alt="pop up"
+                  />
+                  <p className="font-bold px-2 py-8">{item.title}</p>
                 </div>
               ))}
           </div>
@@ -46,5 +51,5 @@ export default () => {
         <Footer />
       </div>
     </Layout>
-  )
-}
+  );
+};
--- server/deploy-jati.sh
+++ server/deploy-jati.sh
@@ -1,8 +1,8 @@
 tar czf jati-api.tar.gz src
-scp jati-api.tar.gz ubuntu@159.65.1.92:./app
+scp jati-api.tar.gz ubuntu@206.189.145.169:./app
 rm jati-api.tar.gz
 
-ssh ubuntu@159.65.1.92 <<'ENDSSH'
+ssh ubuntu@206.189.145.169 <<'ENDSSH'
   cd app
   tar xf jati-api.tar.gz
   rm jati-api.tar.gz
